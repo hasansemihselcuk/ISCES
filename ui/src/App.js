a0e982fadfe0a8cf035416ca1fe36d050de2206f
İnan Deniz Bal <inandeniz.bal@gmail.com>
2023-05-16 18:47:36 +0300

    makeApp
    ilk halini attım kalanı da gelince yapacağım

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
 import LogInPage from "./pages/LogInPage";
+
 function App() {
   return (
     <div className="">
--- ui/src/pages/HomePage.js
+++ ui/src/pages/HomePage.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import MakeApplication from "./MakeApplication";
 const HomePage = () => {
   return (
     <div className="">
+      <MakeApplication />
       <p>sadasa</p>
     </div>
   );
