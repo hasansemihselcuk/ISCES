16bab480c79b23ce2d7e26ab056d2527cab25625
Yusuf Emre Beskan <yusufemre140@gmail.com>
2023-05-16 16:35:33 +0300

    added template

---
+++ ui/src/pages/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="">
+      <p>sadasa</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ ui/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      placeholder: "#242038",
+      uyeol: "#F7ECE1",
+      navbardropdown: "#242038",
+      footerHover: "#242038",
+      white: "#ffffff",
+      inputbox: "#CAC4CE",
+      purple: "#725AC1",
+      lightpurple: "#8D86C9",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
