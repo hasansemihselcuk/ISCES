40c1d1748a776c085caeb27713491c6e4dd073e2
Yusuf Emre Beskan <yusufemre140@gmail.com>
2023-05-19 14:52:09 +0300

    Update SideBar.js

--- ui/src/components/SideBar.js
+++ ui/src/components/SideBar.js
@@ -5,6 +5,7 @@ import "./SideBar.css";
 
 const SideBar = () => {
   const navigate = useNavigate();
+  const authCtx = useContext(AuthContext);
   return (
     <ul className="bg-zinc-800 absolute  mt-10 -ml-9  shadow-lg rounded-md  text-black  scale-125 z-10 text-sm side-bar p-2">
       <li className="mb-2 bg-gray-100 hover:bg-gray-300 ml-4 mr-4 mb-8 mt-4">
@@ -42,6 +43,17 @@ const SideBar = () => {
             Seçim Sonuçları
           </button>
         </Link>
+      </li>
+      <li className="mb-5 bg-gray-100 hover:bg-gray-300 ml-4 mr-4 mt-8">
+        <button
+          onClick={() => {
+            authCtx.onLogout();
+            navigate("/login");
+          }}
+          className="w-full text-left p-2 "
+        >
+          Çıkış yap
+        </button>
       </li>
     </ul>
   );
