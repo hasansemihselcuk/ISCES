36e83b7c18a353d4f406e1f82895bb26eb6f9699
Kağan Yalım <kaganyalim19@gmail.com>
2023-05-19 16:47:49 +0300

    adminlogin and studentlogin fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const Student = require("../models/studentModel");
+const Admin = require("../models/adminModel");
 const catchAsync = require("./../utils/catchAsync");
 const Election = require("../models/departmentElectionModel");
 const AppError = require("./../utils/appError");
@@ -48,30 +49,51 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!email || !password) {
     return next(new AppError("Please provide email and password!", 400));
   }
+
   const student = await Student.findOne({ iztechMail: email }).select(
     "+password"
   );
-  if (!student) {
-    return next(new AppError("Incorrect email or password", 401));
+
+  if (student) {
+    const isPasswordCorrect = student.password === password;
+    if (isPasswordCorrect) {
+      const token = signToken(student._id);
+
+      return res.status(200).json({
+        status: "success",
+        token,
+        sid: student._id,
+        name: student.name,
+        surname: student.surname,
+        studentNumber: student.studentNumber,
+        department: student.department,
+        iztechMail: student.iztechMail,
+        isCandidate: student.isCandidate,
+      });
+    }
   }
 
-  const isPasswordCorrect = student.password === password;
-  if (!isPasswordCorrect) {
-    return next(new AppError("Incorrect email or password", 401));
-  }
-  const token = signToken(student._id);
+  const admin = await Admin.findOne({ iztechMail: email }).select("+password");
+  if (admin) {
+    console.log(email, password);
+    const isPasswordCorrect = admin.password === password;
+    if (isPasswordCorrect) {
+      const token = signToken(admin._id);
 
-  return res.status(200).json({
-    status: "success",
-    token,
-    sid: student._id,
-    name: student.name,
-    surname: student.surname,
-    studentNumber: student.studentNumber,
-    department: student.department,
-    iztechMail: student.iztechMail,
-    isCandidate: student.isCandidate,
-  });
+      return res.status(200).json({
+        status: "success",
+        token,
+        aid: admin._id,
+        name: admin.name,
+        surname: admin.surname,
+        email: admin.iztechMail,
+        isAdmin: admin.isAdmin,
+      });
+    }
+  }
+
+
+  return next(new AppError("Incorrect email or password", 401));
 });
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
@@ -90,18 +112,24 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-  const currentUser = await Student.findById(decoded.id);
-  if (!currentUser) {
-    return next(
-      new AppError(
-        "The user belonging to this token does no longer exist.",
-        401
-      )
-    );
+
+  const currentUserStudent = await Student.findById(decoded.id);
+  if (currentUserStudent) {
+    req.user = currentUserStudent;
+    return next();
   }
 
-  req.user = currentUser;
-  next();
+
+  const currentUserAdmin = await Admin.findById(decoded.id);
+  if (currentUserAdmin) {
+    req.user = currentUserAdmin;
+    return next();
+  }
+
+
+  return next(
+    new AppError("The user belonging to this token does no longer exist.", 401)
+  );
 });
 
 exports.checkElectionStatus = catchAsync(async (req, res, next) => {
