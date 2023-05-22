d755b818a01059ea12c8cbf4d25ac7ff84881123
Yusuf Emre Beskan <yusufemre140@gmail.com>
2023-05-22 21:57:31 +0300

    Update VotingPage.js

--- ui/src/pages/VotingPage.js
+++ ui/src/pages/VotingPage.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import CandidateForVote from "../components/votingPageComponents/candidateForVote";
 import resim from "../components/votingPageComponents/defaultUser.png";
 
@@ -8,6 +9,23 @@ const VotingPage = ({
   voteDepartmentCandidate,
   student,
 }) => {
+  const [candidateList, setCandidates] = useState([]);
+
+  useEffect(() => {
+    // Simulating data received from the backend
+    const studentId = localStorage.getItem("sid");
+    axios
+      .get(`http://localhost:3001/api/v1/student/${studentId}`)
+      .then((res) => {
+        console.log(res.data.data.candidatesFromStudentsDepartment);
+        setCandidates(
+          res.data.data.candidatesFromStudentsDepartment.map((student) => {
+            return { ...student.studentInfos, photo: resim };
+          })
+        );
+      });
+  }, []);
+
   const candidates = [
     {
       name: "Gökhan Türkmen",
@@ -41,7 +59,7 @@ const VotingPage = ({
         <h1>student.department Bölüm Temsilciliği Seçimi</h1>
       </div>
       <div className="p-8 mb-8 rounded-lg relative text-3xl ">
-        <CandidateForVote candidates={candidates} />
+        <CandidateForVote candidates={candidateList} />
       </div>
     </div>
   );
