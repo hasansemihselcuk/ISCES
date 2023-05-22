c0d0b22704c97e43493f999cbbcf0c500701895b
VeeTora <hss.2277@hotmail.com>
2023-05-22 22:42:47 +0300

    navbar düzenleme

--- ui/src/components/NavBar.js
+++ ui/src/components/NavBar.js
@@ -5,6 +5,7 @@ import bellIcon from "./nav-bar-pictures/bell_icon.png";
 import SideBar from "./SideBar";
 import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
+import menu from "./nav-bar-pictures/menu.png";
 
 const NavBar = () => {
   const [showSideBar, setShowSideBar] = useState(false);
@@ -26,20 +27,10 @@ const NavBar = () => {
                   onClick={toggleSideBar}
                   className="relative w-10 h-10 mt-8 mr-2"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-10 h-10 mt-1"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                  </svg>
+                  <img
+                    src={menu}
+                    className="w-10 h-10 rounded-full overflow-hidden"
+                  ></img>
                   <div className="">{showSideBar && <SideBar />}</div>
                 </button>
               </div>
@@ -61,7 +52,10 @@ const NavBar = () => {
               <img src={darkMode} className="w-8 h-8"></img>
             </button>
             <button className="">
-              <img src={turkishFlag} className="w-16 h-16"></img>
+              <img
+                src={turkishFlag}
+                className="w-14 h-14 rounded-full overflow-hidden"
+              ></img>
             </button>
           </div>
         </div>
