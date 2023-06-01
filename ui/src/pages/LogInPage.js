d659abe2019afccda7025f69d9080edcf63900b3
Yusuf Emre Beskan <yusufemre140@gmail.com>
2023-06-01 20:10:39 +0300

    Update LogInPage.js

--- ui/src/pages/LogInPage.js
+++ ui/src/pages/LogInPage.js
@@ -8,6 +8,7 @@ const LogInPage = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
   const [formIsValid, setFormIsValid] = useState(false);
+  const [incorrectData, setIncorrectData] = useState(false);
 
   const authCtx = useContext(AuthContext);
   const navigate = useNavigate();
@@ -47,76 +48,96 @@ const LogInPage = () => {
     const signInInfo = { email: enteredEmail, password: enteredPassword };
 
     event.preventDefault();
-    try {
-      //"http://localhost:3001/api/v1/student/login",
-      const res = await axios.post(
-        "http://localhost:3001/api/v1/student/login",
-        JSON.stringify(signInInfo)
-      );
+    if (formIsValid) {
+      try {
+        //"http://localhost:3001/api/v1/student/login",
+        const res = await axios.post(
+          "http://localhost:3001/api/v1/student/login",
+          JSON.stringify(signInInfo)
+        );
 
-      if (res.data.status === "success") {
-        console.log(res.data);
-        if (res.data.isCandidate) {
-          authCtx.handleCandidate();
-        }
-        if (res.data.isAdmin) {
-          authCtx.handleAdmin();
-          localStorage.setItem("aid", res.data.aid);
-          localStorage.setItem("adminInfo", JSON.stringify(res.data));
+        if (res.data.status === "success") {
+          console.log(res.data);
+          if (res.data.isCandidate) {
+            authCtx.handleCandidate();
+          }
+          if (res.data.isAdmin) {
+            authCtx.handleAdmin();
+            localStorage.setItem("aid", res.data.aid);
+            localStorage.setItem("adminInfo", JSON.stringify(res.data));
+          } else {
+            localStorage.setItem("sid", res.data.sid);
+            localStorage.setItem("studentInfo", JSON.stringify(res.data));
+          }
+
+          authCtx.onLogin();
+          navigate("/");
         } else {
-          localStorage.setItem("sid", res.data.sid);
-          localStorage.setItem("studentInfo", JSON.stringify(res.data));
+          setIncorrectData(true);
         }
-
-        authCtx.onLogin();
-        navigate("/");
-      } else {
-        console.log("Wrong password or email");
+        // assump log in is successful
+      } catch (err) {
+        setIncorrectData(true);
       }
-      // assump log in is successful
-    } catch (err) {
-      console.log(err);
+    } else {
+      setIncorrectData(true);
     }
   };
   return (
-    <div className="grid grid-cols-2 gap-4 ">
-      <div></div>
-      <div className="place-self-end bg-gray-100 w-80 py-20 ml-16">
-        <img src={iyteLogo} className="w-40 h-40 ml-16 mb-8"></img>
-
-        <div className="mb-72">
-          <input
-            onChange={emailChangeHandler}
-            type="text"
-            placeholder="Enter a email"
-            className="mt-12 ml-12 w-48 h-8 px-4"
-          />
-          <input
-            onChange={passwordChangeHandler}
-            type="text"
-            placeholder="Enter a password"
-            className="mt-4 ml-12 w-48 px-4 h-8"
-          />
-          <div className="flex mt-4">
-            <div
-              onClick={rememberMe}
-              className={
-                isRememberUser
-                  ? "w-5 h-5 bg-rose-700 border-2 ml-16 mt-1"
-                  : "w-5 h-5 border-rose-700 border-2 ml-16 mt-1"
-              }
-            ></div>
-            <p className="ml-5">Beni Hatırla</p>
-          </div>
+    <div>
+      {incorrectData && (
+        <div className="bg-gray-100 border-gray-100 border-3 p-8 text-center">
+          <p className="text-3xl">Incorrect email or password</p>
           <button
-            onClick={submitHandler}
-            className="mt-8 ml-16 mb-12 w-40 h-10 border-rose-700 border-2 hover:bg-red-700 rounded-lg"
-            disabled={!formIsValid}
+            className="w-40 h-10 border-rose-700 border-2 hover:bg-red-700 rounded-lg mt-8"
+            onClick={() => {
+              setIncorrectData(false);
+            }}
           >
-            Submit
+            Okay
           </button>
         </div>
-      </div>
+      )}
+      {!incorrectData && (
+        <div className="grid grid-cols-2 gap-4 ">
+          <div></div>
+          <div className="place-self-end bg-gray-100 w-80 py-20 ml-16">
+            <img src={iyteLogo} className="w-40 h-40 ml-16 mb-8"></img>
+
+            <div className="mb-72">
+              <input
+                onChange={emailChangeHandler}
+                type="text"
+                placeholder="Enter a email"
+                className="mt-12 ml-12 w-48 h-8 px-4"
+              />
+              <input
+                onChange={passwordChangeHandler}
+                type="password"
+                placeholder="Enter a password"
+                className="mt-4 ml-12 w-48 px-4 h-8"
+              />
+              <div className="flex mt-4">
+                <div
+                  onClick={rememberMe}
+                  className={
+                    isRememberUser
+                      ? "w-5 h-5 bg-rose-700 border-2 ml-16 mt-1"
+                      : "w-5 h-5 border-rose-700 border-2 ml-16 mt-1"
+                  }
+                ></div>
+                <p className="ml-5">Beni Hatırla</p>
+              </div>
+              <button
+                onClick={submitHandler}
+                className="mt-8 ml-16 mb-12 w-40 h-10 border-rose-700 border-2 hover:bg-red-700 rounded-lg"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
