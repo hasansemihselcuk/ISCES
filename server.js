76e32d39bd402beacf1436bcf12228605fe525fd
VeeTora <hss.2277@hotmail.com>
2023-06-07 13:08:41 +0300

    deploy backend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-const port = 3001;
+const port = process.env.PORT || 3000;
 const app = require("./App");
 const hostname = "localhost";
 
